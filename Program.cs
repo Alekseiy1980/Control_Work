c2488b870ac7e386bc75afbdaaad9e2788ee9012
Alekseiy1980 <129889765+Alekseiy1980@users.noreply.github.com>
2023-06-21 20:37:34 +0300

    Программа v 0.0.1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿
+int size;
+Console.WriteLine("enter the text of the word, separate with a space, as soon as you finish typing, press enter");
+string? text = Console.ReadLine();
+string[] words = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+do
+{
+   Console.WriteLine("Enter the size of the text you want to output - ");
+   size = Convert.ToInt32(Console.ReadLine());
+   if (size <= 0)
+   {
+      Console.WriteLine("incorrect data entered");
+      Console.Clear();
+   }
+} while (size <= 0);
+
+string[] word = Array.FindAll(words, word => word.Length <= size);
+
+for (int i = 0; i < word.Length; i++)
+{
+   Console.Write(word[i] + " ");
+}
